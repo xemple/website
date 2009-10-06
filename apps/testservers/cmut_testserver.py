762b7b58673607ed3095580300c0c4e7e4e64fb8
Andrew DE MURGA <ademurga@Akira.local>
2009-10-06 15:55:01 +0200

    deletion test

--- apps/testservers/cmut_testserver.py
+++
@@ -1,6 +0,0 @@
-from random import choice
-
-def pay(amount):
-	length=10 
-	allowed_chars='abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789'
-	return True, amount, ''.join([choice(allowed_chars) for i in range(length)])
